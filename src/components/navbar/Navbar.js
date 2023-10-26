64ab28ee605360286c0d9048e5736dd962833b53
Karan Joshi <Karan190213@gmail.com>
2023-10-26 16:16:18 +0530

    NavBar Updated

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,44 +1,25 @@
-import React, { useState } from 'react';
-import { Layout, Switch, Input } from 'antd';
-import { SearchOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import './Navbar.scss'; // Import your SASS file
+import React from 'react';
+import { Layout, Switch } from 'antd';
+import { BellOutlined, SettingOutlined } from '@ant-design/icons';
+import './Navbar.scss';
 
 const { Header } = Layout;
 
 const Navbar = ({ darkMode, toggleDarkMode }) => {
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
-
-  const handleSearchIconClick = () => {
-    setIsSearchOpen(!isSearchOpen);
-  };
-
   return (
     <Header className="navbar">
       <div className="logo">
         <h1>Stock Screener</h1>
       </div>
       <div className="right-menu">
-        <div className={`search-container ${isSearchOpen ? 'open' : ''}`}>
-          <Input
-            placeholder="Search..."
-            prefix={<SearchOutlined />}
-            allowClear
-            style={{ display: isSearchOpen ? 'block' : 'none' }}
-          />
-        </div>
-        <button
-          onClick={handleSearchIconClick}
-          style={{ visibility: isSearchOpen ? 'hidden' : 'visible' }}
-        >
-          <SearchOutlined />
-        </button>
         <Switch
           checked={darkMode}
           onChange={toggleDarkMode}
           checkedChildren="Dark"
           unCheckedChildren="Light"
         />
-        <InfoCircleOutlined className="about-icon" />
+        <BellOutlined className="notification-icon" /> {/* Notification Icon */}
+        <SettingOutlined className="settings-icon" /> {/* Settings Icon */}
       </div>
     </Header>
   );
