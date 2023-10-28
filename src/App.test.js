2965fc34aa41c0048164f1d0254e7e69e93c60a1
Karan Joshi <Karan190213@gmail.com>
2023-10-28 17:19:29 +0530

    Test case implemant

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render } from '@testing-library/react';
+import Navbar from './components/navbar/Navbar';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Navbar component renders without errors', () => {
+  const { container } = render(<Navbar darkMode toggleDarkMode={() => {}} />);
+  expect(container).toBeTruthy();
 });
